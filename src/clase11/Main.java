7095d1fe15e12735d611f08b17fd4b07d301414f
Christian Mansilla <cmansilla@issd.edu.ar>
2024-11-08 18:54:17 -0300

    v2 - Agregado impresion de mensaje y variable nombre

--- src/clase11/Main.java
+++ src/clase11/Main.java
@@ -3,6 +3,10 @@ package clase11;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo - Bienvenidos a Git");
+		
+		String nombre = "Christian";
 
 	}
 
